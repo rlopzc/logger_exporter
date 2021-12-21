b3888b1e3033746e608e3f24762cf0a77d8eeb1e
Romario López <romariolopezc@gmail.com>
2021-12-21 13:23:49 -0600

    Add exporter behaviour

---
+++ lib/logger_exporter/exporter.ex
@@ -0,0 +1,22 @@
+defmodule LoggerExporter.Exporter do
+  @moduledoc """
+  Behaviour that should be implemented by exporters.
+  Example implementation in `LoggerExporter.Exporters.LokiExporter`
+  """
+
+  alias LoggerExporter.Event
+
+  @doc """
+  Authorization headers
+
+  Content-Type: application/json is added by default
+  """
+  @callback headers() :: Mint.Types.headers()
+
+  @doc """
+  Body to sent to the external service
+
+  The body will be encoded by the json library
+  """
+  @callback body([Event.t()]) :: term()
+end
