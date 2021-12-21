450ad97c827e03fafdee6b4cde153abaf9383558
Romario López <romariolopezc@gmail.com>
2021-12-21 13:23:32 -0600

    Add formatter behaviour

---
+++ lib/logger_exporter/formatter.ex
@@ -0,0 +1,17 @@
+defmodule LoggerExporter.Formatter do
+  @moduledoc """
+  Behaviour that should be implemented by log formatters.
+  Example implementation in `LoggerExporter.Formatters.BasicLogger`
+  """
+
+  @doc """
+  Format event callback.
+  """
+  @callback format_event(
+              level :: Logger.level(),
+              msg :: Logger.message(),
+              ts :: Logger.Formatter.time(),
+              md :: keyword(),
+              md_keys :: [atom] | :all
+            ) :: String.t()
+end
