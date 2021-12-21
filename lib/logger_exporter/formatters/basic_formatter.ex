5d43877d474b1d774358a67d8e98d220510876d5
Romario López <romariolopezc@gmail.com>
2021-12-21 13:24:02 -0600

    Add BasicFormatter

---
+++ lib/logger_exporter/formatters/basic_formatter.ex
@@ -0,0 +1,10 @@
+defmodule LoggerExporter.Formatters.BasicFormatter do
+  @behaviour LoggerExporter.Formatter
+
+  def format_event(level, msg, ts, md, md_keys) do
+    "$time [$level] $message $metadata"
+    |> Logger.Formatter.compile()
+    |> Logger.Formatter.format(level, msg, ts, LoggerExporter.take_metadata(md, md_keys))
+    |> IO.chardata_to_string()
+  end
+end
