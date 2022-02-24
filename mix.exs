e2fae5f3a165e6e2aaf8465d8cd8cadbcf23b82b
Romario López <romariolopezc@gmail.com>
2022-02-23 22:20:52 -0600

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule LoggerExporter.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/romariolopezc/logger_exporter"
-  @version "0.1.1"
+  @version "0.2.0"
 
   def project do
     [
