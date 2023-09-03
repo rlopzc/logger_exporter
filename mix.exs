162444b437bcb99681ea1dd311729caa713baa59
Romario López <romariolopezc@gmail.com>
2023-09-03 10:26:13 +0200

    Add dialyxir

--- mix.exs
+++ mix.exs
@@ -14,7 +14,13 @@ defmodule LoggerExporter.MixProject do
       name: "LoggerExporter",
       source_url: @source_url,
       package: package(),
-      docs: docs()
+      docs: docs(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+        plt_core_path: "priv/plts",
+        plt_add_deps: :app_tree,
+        plt_add_apps: [:mix, :plug]
+      ]
     ]
   end
 
@@ -25,11 +31,12 @@ defmodule LoggerExporter.MixProject do
   defp deps do
     [
       {:req, "~> 0.4.0"},
-      {:jason, "~> 1.2"},
+      {:jason, "~> 1.2", optional: true},
       {:telemetry, "~> 0.4.2 or ~> 1.0"},
-      {:plug, "~> 1.7"},
+      {:plug, "~> 1.7", optional: true},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.3", only: [:test, :dev], runtime: false}
     ]
   end
 
