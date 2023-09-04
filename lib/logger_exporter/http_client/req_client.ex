42756453a1162c570704d36fc06e7b42a8ef90a0
Romario López <romariolopezc@gmail.com>
2023-09-04 11:08:18 +0200

    Update ReqClient to comply with spec
    Req 0.4.x updates its headers to a map instead of list

--- lib/logger_exporter/http_client/req_client.ex
+++ lib/logger_exporter/http_client/req_client.ex
@@ -1,17 +1,38 @@
 defmodule LoggerExporter.HttpClient.ReqClient do
   @moduledoc """
   HTTP Client implementing `LoggerExporter.HttpClient` using `Req`.
+
+  https://hexdocs.pm/req/Req.html
   """
   @behaviour LoggerExporter.HttpClient
 
   def post(url, headers, body) do
-    case Req.post(url, headers: headers, body: body) do
+    normalized_headers = normalize_headers(headers)
+
+    case Req.post(url, headers: normalized_headers, body: body) do
       {:ok, %Req.Response{status: status, headers: headers, body: body}} ->
-        headers = Enum.into(headers, [])
-        {:ok, status, headers, body}
+        denormalized_headers = denormalize_headers(headers)
+        {:ok, status, denormalized_headers, body}
 
       {:error, exception} ->
         {:error, exception}
     end
+  end
+
+  # Change `LoggerExporter.HttpClient.headers()` list to map
+  defp normalize_headers(headers) do
+    Enum.reduce(headers, %{}, fn {header, value}, map ->
+      header = String.downcase(header)
+
+      Map.update(map, header, [value], &[value | &1])
+    end)
+  end
+
+  # Deduplicate headers and return the last value of the headers list
+  defp denormalize_headers(headers) do
+    Enum.reduce(headers, [], fn {header, values}, list ->
+      [{header, List.last(values)} | list]
+    end)
+    |> Enum.reverse()
   end
 end
