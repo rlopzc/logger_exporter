97528dd5b08802525698e0ca8124ec1e614511e9
Romario López <romariolopezc@gmail.com>
2023-12-08 19:00:33 -0600

    Fix event fixtures to use attrs

--- test/support/fixtures/event_fixtures.ex
+++ test/support/fixtures/event_fixtures.ex
@@ -6,12 +6,14 @@ defmodule LoggerExporter.EventFixtures do
   alias LoggerExporter.Event
 
   def event_fixture(attrs \\ %{}) do
-    %Event{
+    attrs
+    |> Enum.into(%{
       timestamp_ns: System.os_time(:nanosecond),
       log_line: "Log line",
       level: :info,
       app_name: "logger_exporter_app",
       metadata: [user_id: 123, request_id: "uuid"]
-    }
+    })
+    |> then(&struct!(Event, &1))
   end
 end
