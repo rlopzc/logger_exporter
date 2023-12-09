13afac26f0009eb6c60f9914bb4fffe00dcdd9ed
Romario López <romariolopezc@gmail.com>
2023-12-08 18:57:55 -0600

    LokiExporter Test WIP

--- test/logger_exporter/exporters/loki_exporter_test.exs
+++ test/logger_exporter/exporters/loki_exporter_test.exs
@@ -2,4 +2,10 @@ defmodule LoggerExporter.Exporters.LokiExporterTest do
   use ExUnit.Case, async: true
 
   alias LoggerExporter.Exporters.LokiExporter
+
+  import LoggerExporter.EventFixtures
+
+  test "parses events" do
+    events = [event_fixture(), event_fixture()]
+  end
 end
