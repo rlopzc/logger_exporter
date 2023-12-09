610d788df326dd1099068438ec1c3a65ef92401e
Romario López <romariolopezc@gmail.com>
2023-12-08 18:45:31 -0600

    Bump versions

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule LoggerExporter.MixProject do
     [
       app: :logger_exporter,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       deps: deps(),
       description: "Export your logs to the service of your choice.",
       name: "LoggerExporter",
