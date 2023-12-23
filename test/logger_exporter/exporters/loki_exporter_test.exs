7d4523796be61a10534ed5cca8a6c03b0e9bb015
Romario López <romariolopezc@gmail.com>
2023-12-22 18:15:57 -0600

    Add loki exporter test

--- test/logger_exporter/exporters/loki_exporter_test.exs
+++ test/logger_exporter/exporters/loki_exporter_test.exs
@@ -5,7 +5,35 @@ defmodule LoggerExporter.Exporters.LokiExporterTest do
 
   import LoggerExporter.EventFixtures
 
-  test "parses events" do
-    events = [event_fixture(), event_fixture()]
+  setup do
+    Application.put_env(:logger, LoggerExporter, app_name: :test_app, environment_name: :test)
+    :ok
+  end
+
+  describe "headers" do
+    test "application/json" do
+      assert [{"Content-Type", "application/json"}] == LokiExporter.headers()
+    end
+  end
+
+  describe "body" do
+    test "parses events and encodes it" do
+      events = [event_fixture(), event_fixture()]
+
+      assert %{
+               streams: [
+                 %{
+                   stream: %{
+                     app: "test_app",
+                     env: "test"
+                   },
+                   values: [
+                     [_timestamp1, "Log line"],
+                     [_timestamp2, "Log line"]
+                   ]
+                 }
+               ]
+             } = Jason.decode!(LokiExporter.body(events), keys: :atoms)
+    end
   end
 end
